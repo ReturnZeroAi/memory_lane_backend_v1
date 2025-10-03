3fd08fd991dbeddc399742fab3d043c5a25de701
Bhuvanesh24 <bhuvaneshwaran813@gmail.com>
2025-10-03 22:42:15 +0530

    fix in structure

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import authRoutes from '../modules/auth/controllers/routes';
-import userRoutes from '../modules/users/controllers/routes';
-import laneRoutes from '../modules/lanes/controllers/routes';
-import calendarRoutes from '../modules/calendar/controllers/routes';
-import notesRoutes from '../modules/notes/controllers/routes';
+import authRoutes from '@auth/controllers/routes';
+import userRoutes from '@users/controllers/routes';
+import laneRoutes from '@lanes/controllers/routes';
+import calendarRoutes from '@calendar/controllers/routes';
+import notesRoutes from '@notes/controllers/routes';
 
 const router = Router();
+
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
--- jest.config.cjs
+++ jest.config.cjs
@@ -3,5 +3,12 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   collectCoverageFrom: ['apps/api/src/**/*.{ts,tsx}'],
-  moduleNameMapper: { '^@common/(.*)$': '<rootDir>/libs/common/src/$1' }
+  moduleNameMapper: {
+    '^@common/(.*)$': '<rootDir>/libs/common/src/$1',
+    '^@auth/(.*)$': '<rootDir>/apps/api/src/auth/$1',
+    '^@users/(.*)$': '<rootDir>/libs/users/$1',
+    '^@lanes/(.*)$': '<rootDir>/libs/lanes/$1',
+    '^@calendar/(.*)$': '<rootDir>/libs/calendar/$1',
+    '^@notes/(.*)$': '<rootDir>/libs/notes/$1'
+  }
 };
