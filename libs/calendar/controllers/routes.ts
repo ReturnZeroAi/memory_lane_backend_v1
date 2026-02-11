9a95d781072f1d43288111d8710bca16632bcb41
Bhuvanesh24 <bhuvaneshwaran813@gmail.com>
2026-02-11 11:23:10 +0530

    Deleted the codebase to reinitialize correctly

--- libs/calendar/controllers/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-// TODO: add endpoints
-router.get('/health', (_req, res) => res.json({ ok: true }));
-
-export default router;
--- libs/shared/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './errors/httpError';
-export * from './logger';
